b389cf9a84e7bf8e464530b2c869414fddc46d5a
tc chen <tc.chen@insyde.com>
2026-04-17 08:33:17 +0800

    註冊 RemittanceService 到 DI 容器中

--- RemittanceTest/Program.cs
+++ RemittanceTest/Program.cs
@@ -1,8 +1,14 @@
+using RemittanceTest.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Register RemittanceService, TC+++
+builder.Services.AddSingleton<IRemittanceService, RemittanceService>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
