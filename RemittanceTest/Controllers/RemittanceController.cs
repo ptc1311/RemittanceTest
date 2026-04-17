47cd0c6b3ef866bf03b32b7a05b52d4ff0caed52
tc chen <tc.chen@insyde.com>
2026-04-17 08:46:14 +0800

    Controller實作:驗證input/call service/mapping http code

--- RemittanceTest/Controllers/RemittanceController.cs
+++ RemittanceTest/Controllers/RemittanceController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RemittanceTest.Models;
+using RemittanceTest.Services;
 
 namespace RemittanceTest.Controllers
 {
@@ -7,15 +8,37 @@ namespace RemittanceTest.Controllers
     [Route("api/[controller]")]
     public class RemittanceController : ControllerBase
     {
+        private readonly IRemittanceService _service; //TC+++
+
         // TODO: 1. 請透過建構子注入 (Constructor Injection) 引入 IRemittanceService
+        // Constructor Injection, TC+++:start
+        public RemittanceController(IRemittanceService service)
+        {
+            _service = service;
+        } //TC+++:end
 
         [HttpPost("{id}/cancel")]
         public IActionResult Cancel(int id)
         {
             // TODO: 2. 呼叫 Service 執行取消邏輯
             // TODO: 3. 根據 Service 回傳的結果，回傳相對應的 HTTP 狀態碼 (Ok / BadRequest / NotFound)
+            // tc+++:start
+            if (id <= 0)
+                return BadRequest("Invalid id");
 
-            return Ok();
+            var (isSuccess, message) = _service.CancelRemittance(id);
+
+            if (isSuccess)
+                return Ok(new { message = "Cancelled successfully" });
+
+            return message switch
+            {
+                "NOT_FOUND" => NotFound(), //404
+                "INVALID_STATUS" => BadRequest("Status not allowed"), //400
+                _ => StatusCode(500)
+            };
+            //tc+++:end
+            //return Ok();
         }
     }
 }
